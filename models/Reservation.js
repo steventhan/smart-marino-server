b57c785a658394f18cd7b5defab8b9675b74f3ef
Steven Than <steventhan11@gmail.com>
2018-04-04 03:19:25 -0400

    fixed typo in model name, make start and end field full blown date objects

--- models/Reservation.js
+++ models/Reservation.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReservationSchema = new Schema({
-  date: { type: Date, required: true },
-  start: { type: String, required: true },
-  end: { type: String, required: true },
+  start: { type: Date, required: true },
+  end: { type: Date, required: true },
   machine: { type: Number, ref: "Machine", required: true },
-  userId: { type: String, required: true },
+  user: { type: String, required: true },
+  status: { type: String, required: true }
 });
 
-const Reservation = mongoose.model('Revservation', ReservationSchema);
+const Reservation = mongoose.model("Reservation", ReservationSchema);
 
 module.exports = Reservation;
